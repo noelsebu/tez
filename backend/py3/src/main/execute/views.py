4970f5c5f9732f31b0415fc38f2d201757ee79f8
noelsebu <noelsebastian721@gmail.com>
2019-08-13 16:38:32 +0530

    demo

--- backend/py3/src/main/execute/views.py
+++ backend/py3/src/main/execute/views.py
@@ -33,16 +33,17 @@ class cd:
 
 def testcases_list(request):
         if (request.method == 'POST'):
-            time.sleep(3)
+            time.sleep(30)
             #print(request.session['mie'])
             
             
             print(request.data[0])
             print(request.data[0]['testid'])
             i=0
+            testscriptjavamie=[]
             testscriptjava=[]   
             testscriptpy=[]
-            
+            flag=0
             #test for java or python cases
              
             
@@ -50,29 +51,70 @@ def testcases_list(request):
                 word=str(request.data[i]['testscript'])
                 if (word.find('attrqa') != -1): 
                     print ("Contains given substring ")
-                    testscriptjava.append(request.data[i]['testscript']) #append to java testcases
+                    if(word.find('mie') != -1):
+                        flag=1 
+                        testscriptjavamie.append(request.data[i]['testscript']) #append to java testcases
+                    else:
+                        testscriptjava.append(request.data[i]['testscript'])
+                        if(flag ==1):
+                            flag=2
                 else:
                     testscriptpy.append(request.data[i]['testscript']) #appends to python testcases
                 i=i+1
             print(testscriptjava)
             i=0
-            if testscriptjava: #execute java cases
+            if (testscriptjava or testscriptjavamie): #execute java cases
                 with cd("/home/noel/testmate1/attr-qa-api-ui-auto"):
                     j=0
                                     
                     
                     #subprocess.call(['git', 'clone',giturl])
-                    file1 = open("./testjava.xml","w")
-                    startingheaders =['<?xml version="1.0" encoding="UTF-8"?> \n','<!DOCTYPE suite SYSTEM "http://testng.org/testng-1.0.dtd">\n',
-                    '<suite name="SuiteName"> \n','<test  name="ValidToken"> \n','<classes>']
-                    endingfooter=['</classes> \n','</test> \n','</suite> \n']     
-                    file1.writelines(startingheaders)
-                    while j < len(testscriptjava):
-                        file1.write('<class name="'+testscriptjava[j] +'"/>')
-                        file1.write("\n")
-                        j=j+1 
-                    file1.writelines(endingfooter)
-                    file1.close()
+                    file1 = open("./testjava.xml","a")
+                    #startingheaders =['<?xml version="1.0" encoding="UTF-8"?> \n','<!DOCTYPE suite SYSTEM "http://testng.org/testng-1.0.dtd">\n',
+                    #'<suite name="SuiteName"> \n','<test  name="ValidToken"> \n','<classes>']
+                    endingfooter=['</classes> \n','</test> \n','</suite> \n']
+                    endingfootermie=['</classes> \n','</test> \n']     
+                    #file1.writelines(startingheaders)
+
+                    if(flag==0):
+                        while j < len(testscriptjava):
+                            file1.write('<class name="'+testscriptjava[j] +'"/>')
+                            file1.write("\n")
+                            j=j+1 
+                        file1.writelines(endingfooter)
+                        file1.close()
+                    
+                    elif(flag==1):
+                        while j < len(testscriptjavamie):
+                            file1.write('<class name="'+testscriptjavamie[j] +'"/>')
+                            file1.write("\n")
+                            j=j+1 
+                        file1.writelines(endingfooter)
+                        file1.close()
+                    
+                    elif(flag==2):
+                        k=0
+                        while j < len(testscriptjavamie):
+                            file1.write('<class name="'+testscriptjavamie[j] +'"/>')
+                            file1.write("\n")
+                            j=j+1 
+                        file1.writelines(endingfootermie)
+                        file2 = open("./authtoken.xml","r")
+                        for x in file2.readlines():
+                            file1.write(x)
+                        file2.close()
+                        
+                        while k < len(testscriptjava):
+                            file1.write('<class name="'+testscriptjava[k] +'"/>')
+                            file1.write("\n")
+                            k=k+1
+
+                        file1.writelines(endingfooter)
+                        file1.close()
+
+                    
+                    
+                    
                     subprocess.call(['mvn' ,'clean', 'test' ,'-Dsurefire.suiteXmlFiles=testjava.xml', '-DlogToMongo=y'])
                     cmd = "ls -tr /home/noel/testmate1/attr-qa-api-ui-auto/reports | tail -1"
                     ps=subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)    
@@ -130,22 +172,41 @@ def users(request):
             subprocess.call(['git' , 'pull',giturl])
             
             file1 = open("./testjava.xml","w")
+            file2 = open("./authtoken.xml","w")
             startingheaders =['<?xml version="1.0" encoding="UTF-8"?> \n','<!DOCTYPE suite SYSTEM "http://testng.org/testng-1.0.dtd">\n',
-                    '<suite name="SuiteName"> \n','<test  name="ValidToken"> \n']
+                    '<suite name="SuiteName"> \n']
             file1.writelines(startingheaders)
-            if(str(request.data['mie'])!="none"):
+
+            if((str(request.data['mie']).lower)!="none" and (str(request.data['token']).lower)!="none"):
                 mie_token=str(request.data['mie'])
-                token_headers='<parameter name="authToken" value="' + mie_token + '"></parameter>'
-                class_headers='<classes> \n <class name="attrqa.tests.authtoken.AuthorizationToken_MIE" /> \n'
+                token_headers='<test  name="MIE"> \n<parameter name="authToken" value="' + mie_token + '"></parameter>'
+                class_headers='<classes> \n<class name="attrqa.tests.authtoken.AuthorizationToken_MIE" /> \n'
                 file1.write(token_headers)
                 file1.write(class_headers)
-            if(str(request.data['token'])!="none"):
+
+
+                token=str(request.data['token'])
+                token_headers='<test  name="Token"> \n<parameter name="authToken" value="' +token+ '"></parameter> \n'
+                class_headers='<classes> \n<class name="attrqa.tests.authtoken.AuthorizationToken" /> \n'
+                file2.write(token_headers)
+                file2.write(class_headers)
+
+
+            
+            elif((str(request.data['mie']).lower)!="none"):
+                mie_token=str(request.data['mie'])
+                token_headers='<test  name="MIE"> \n<parameter name="authToken" value="' + mie_token + '"></parameter>'
+                class_headers='<classes> \n<class name="attrqa.tests.authtoken.AuthorizationToken_MIE" /> \n'
+                file1.write(token_headers)
+                file1.write(class_headers)
+            elif((str(request.data['token']).lower)!="none"):
                     token=str(request.data['token'])
-                    token_headers='<parameter name="authToken" value="' +token+ '"></parameter> \n'
-                    class_headers='<classes> \n <class name="attrqa.tests.authtoken.AuthorizationToken" /> \n'
+                    token_headers='<test  name="Token"> \n<parameter name="authToken" value="' +token+ '"></parameter> \n'
+                    class_headers='<classes> \n<class name="attrqa.tests.authtoken.AuthorizationToken" /> \n'
                     file1.write(token_headers)
                     file1.write(class_headers)
 
-        
+            file1.close()
+            file2.close()
         return Response(request.data, status=status.HTTP_200_OK)
     
